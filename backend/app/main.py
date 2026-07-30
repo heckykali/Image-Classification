c46cc04efd73ffba5d37abc3d05342f4ba1c4161
Hecky's World <hecky.kali@gmail.com>
2026-07-30 10:04:45 +0000

    33

--- backend/app/main.py
+++ backend/app/main.py
@@ -115,8 +115,5 @@ if __name__ == "__main__":
     import uvicorn
     settings = get_settings()
     uvicorn.run(
-        "backend.app.main:app",
-        host=settings.host,
-        port=settings.port,
-        reload=settings.debug,
+        "backend.app.main:app"
     )
