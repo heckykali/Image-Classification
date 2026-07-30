30cb5ac69ff53e6f8fb2375d0b7a9378f721ed8a
Hecky's World <hecky.kali@gmail.com>
2026-07-30 10:44:52 +0000

    undo

--- backend/app/main.py
+++ backend/app/main.py
@@ -116,7 +116,5 @@ if __name__ == "__main__":
     settings = get_settings()
     uvicorn.run(
         "backend.app.main:app",
-        host=settings.host,
-        port=settings.port,
         reload=settings.debug,
     )
